7c52ecf1a0acb6dec7bb8a3876611eff6d4390a0
jiz148 <jizjiz148148@gmail.com>
2023-01-03 10:16:22 -0500

    added getSpellList

--- wujiang_api_backend/resources/spell.py
+++ wujiang_api_backend/resources/spell.py
@@ -3,19 +3,54 @@ Spell Resource
 """
 from flask import jsonify, make_response
 from flask_restful import Resource, reqparse, abort
+from sqlalchemy.sql import text
+from sqlalchemy.exc import OperationalError
 
 from wujiang_api_backend.db import db
 from wujiang_api_backend.models.spell import SpellModel, SpellSchema
+
+
+spells_get_args = reqparse.RequestParser()
+spells_get_args.add_argument("pageNum", type=int, required=True)
+spells_get_args.add_argument("pageSize", type=int, required=True)
+spells_get_args.add_argument("sortName", type=str, required=False)
+spells_get_args.add_argument("sortOrder", type=str, required=False)
+spells_get_args.add_argument("name", type=str, required=False)
 
 
 class Spells(Resource):
 
     @staticmethod
     def get():
+        args = spells_get_args.parse_args()
+        page_num = args['pageNum']
+        page_size = args['pageSize']
 
-        all_spells = SpellModel.query.all()
-        result = SpellSchema(many=True).dump(all_spells)
-        return jsonify(result)
+        sort_name = args.get('sortName')
+        sort_order = args.get('sortOrder')
+        name = args.get('name')
+
+        results = db.session.query(SpellModel)
+
+        if sort_name:
+            sort_order = 'desc' if sort_order == 'descend' else 'asc'
+            order_text = sort_name + ' ' + sort_order
+            results = results.order_by(text(order_text))
+
+        if name:
+            name = '%' + name + '%'
+            results = results.filter(SpellModel.spell_name.like(name))
+
+        try:
+            results = results.paginate(page=page_num, per_page=page_size)
+        except OperationalError:
+            abort(400, msg='sql query error')
+        spells = SpellSchema(many=True).dump(results)
+        response = {
+            'total': len(spells),
+            'list': spells
+        }
+        return jsonify(response)
 
 
 spell_post_args = reqparse.RequestParser()
