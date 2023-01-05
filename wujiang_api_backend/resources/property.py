a89c9a4c895fb579d024ed574026fbd84f1e0cfe
jiz148 <jizjiz148148@gmail.com>
2023-01-04 21:01:56 -0500

    minor changes for properties

--- wujiang_api_backend/resources/property.py
+++ wujiang_api_backend/resources/property.py
@@ -1,7 +1,7 @@
 """
 Property Resource
 """
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from flask_restful import Resource, reqparse, abort
 from sqlalchemy.sql import text
 from sqlalchemy.exc import OperationalError
@@ -11,19 +11,11 @@ from wujiang_api_backend.models.property import PropertyModel, PropertySchema
 from wujiang_api_backend.models.unit import UnitModel
 
 
-propertys_get_args = reqparse.RequestParser()
-propertys_get_args.add_argument("pageNum", type=int, required=True)
-propertys_get_args.add_argument("pageSize", type=int, required=True)
-propertys_get_args.add_argument("sortName", type=str, required=False)
-propertys_get_args.add_argument("sortOrder", type=str, required=False)
-propertys_get_args.add_argument("name", type=str, required=False)
-
-
 class Properties(Resource):
 
     @staticmethod
     def get():
-        args = propertys_get_args.parse_args()
+        args = request.args
         page_num = args['pageNum']
         page_size = args['pageSize']
 
@@ -46,10 +38,14 @@ class Properties(Resource):
             results = results.paginate(page=page_num, per_page=page_size)
         except OperationalError:
             abort(400, msg='sql query error')
-        propertys = PropertySchema(many=True).dump(results)
+        properties = PropertySchema(many=True).dump(results)
+        data = {
+            'total': len(properties),
+            'list': properties
+        }
         response = {
-            'total': len(propertys),
-            'list': propertys
+            'code': 200,
+            'data': data
         }
         return jsonify(response)
 
@@ -74,44 +70,47 @@ class Property(Resource):
 
         db.session.add(property)
         db.session.commit()
-        return make_response(PropertySchema().dump(property), 201)
-
-
-property_by_unit_get_args = reqparse.RequestParser()
-property_by_unit_get_args.add_argument("unitId", type=str, required=True)
+        response = {
+            'code': 201,
+            'data': PropertySchema().dump(property)
+        }
+        return make_response(response, 201)
 
 
 class PropertiesByUnit(Resource):
 
     @staticmethod
     def get():
-        args = property_by_unit_get_args.parse_args()
+        args = request.args
         unit_id = args['unitId']
         unit = db.session.query(UnitModel).filter(UnitModel.unit_id == unit_id).first()
         if not unit:
             abort(404, msg='unit does not exist')
         results = unit.property
-        propertys = PropertySchema(many=True).dump(results)
+        properties = PropertySchema(many=True).dump(results)
         response = {
-            'list': propertys
+            'code': 200,
+            'data': {
+                'list': properties
+            }
         }
         return jsonify(response)
-
-
-property_detail_get_args = reqparse.RequestParser()
-property_detail_get_args.add_argument("id", type=str, required=True)
 
 
 class PropertyDetail(Resource):
 
     @staticmethod
     def get():
-        args = property_detail_get_args.parse_args()
+        args = request.args
         property_id = args['id']
         property = db.session.query(PropertyModel).filter(PropertyModel.property_id == property_id).first()
         if not property:
             abort(404, msg='Property does not exist')
-        return jsonify(PropertySchema().dump(property))
+        response = {
+            'code': 200,
+            'data': PropertySchema().dump(property)
+        }
+        return jsonify(response)
 
 
 property_put_args = reqparse.RequestParser()
@@ -138,17 +137,17 @@ class PropertyUpdate(Resource):
         property.property_description = args['description']
 
         db.session.commit()
-        return make_response(PropertySchema().dump(property), 201)
-
-
-property_delete_args = reqparse.RequestParser()
-property_delete_args.add_argument("id", type=str, required=True)
+        response = {
+            'code': 201,
+            'data': PropertySchema().dump(property)
+        }
+        return make_response(response, 201)
 
 
 class PropertyDelete(Resource):
 
     def delete(self):
-        args = property_delete_args.parse_args()
+        args = request.args
         if not db.session.query(PropertyModel).filter(PropertyModel.property_id == args['id']).first():
             abort(404, msg='property does not exist')
         try:
