4ca5f324eac9157d651daca3881d199e548c8fa7
jiz148 <jizjiz148148@gmail.com>
2023-01-09 21:52:41 -0500

    added run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+# add system path
+import sys
+import os
+project_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(project_path)
+from wujiang_api_backend.api import app
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3001)
